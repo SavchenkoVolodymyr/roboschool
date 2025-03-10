8c598401d3937a0cbcf344d91da65b6e5d4f03ac
SavchenkoVolodymyr <v1987savchenko@ukr.net>
2025-03-10 22:03:43 +0200

    feat slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
 const body = document.querySelector('body');
+
+// for slider
+
+function coachesSlider() {
+	let offset = 0;
+	const sliderLine = document.querySelector('.slider__line');
+	let gap = parseInt(getComputedStyle(sliderLine).columnGap);
+	const elemWidht = document.querySelector('.slider__element').clientWidth;
+	const numberOfElem = document.querySelectorAll('.slider__element').length;
+	const btnPrev = document.querySelector('.slider_nav__prew');
+	const btnNext = document.querySelector('.slider_nav__next');
+	btnNext.addEventListener('click', function () {
+		offset = offset + (elemWidht + gap);
+		if (offset > (numberOfElem - 3) * (elemWidht + gap)) {
+			offset = 0;
+		}
+		sliderLine.style.left = -offset + 'px';
+	});
+	btnPrev.addEventListener('click', function () {
+		offset = offset - (elemWidht + gap);
+		if (offset < 0) {
+			offset = (numberOfElem - 3) * (elemWidht + gap);
+		}
+		sliderLine.style.left = -offset + 'px';
+	});
+}
+coachesSlider();
